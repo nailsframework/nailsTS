ab5707cf3be73a7e8b2090b2653fc78c3687191f
Dominic Järmann <dominic.jaermann@icloud.com>
2019-10-25 16:15:20 +0200

    NailsTS

--- modules/injectme.module.js
+++
@@ -1,8 +0,0 @@
-export class Greeter{
-    constructor(){
-    }
-    greet(name){
-        return 'Hi, ' + name;
-    }
-
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
+const path = require('path');
+
 module.exports = {
-    entry: "./main.js",
-    mode: "production",
-    output: {
-      filename: "app.js"
-    }
-  }
+  entry: './src/main.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [ '.js','.tsx', '.ts'],
+  },
+  output: {
+    filename: 'src/app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
